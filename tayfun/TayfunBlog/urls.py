8d28c7fad7c7676dc62f5b36423ced9613465435
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2022-06-01 11:12:30 +0300

    .

--- tayfun/TayfunBlog/urls.py
+++ tayfun/TayfunBlog/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from .views import HomeView, ArtDetail
+
+
+
+
+
+
+
 
 urlpatterns = [
-    path('',views.home, name = "home" ),
+   # path('',views.home, name = "home" ),
+   path('',HomeView.as_view(),name='home'),
+   path('article/<int:pk>',ArtDetail.as_view(),name='article-details'),
 ]
 
--- tayfun/TayfunBlog/views.py
+++ tayfun/TayfunBlog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
+#def home(request):
+ #   return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    
+class ArtDetail(DetailView):
+    model = Post
+    template_name = 'artDet.html'
+    
 
 
 # Create your views here.
