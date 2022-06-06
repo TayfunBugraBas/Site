117aec3612eb60c4c6c29565dfe50c6e32e64c5f
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2022-06-06 22:59:47 +0300

    V1.1.2HerokuReady

--- tayfun/tayfun/settings.py
+++ tayfun/tayfun/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tayfun.urls'
@@ -126,7 +130,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
     
-    ]
+ ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
@@ -135,3 +141,5 @@ LOGOUT_REDIRECT_URL='home'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
